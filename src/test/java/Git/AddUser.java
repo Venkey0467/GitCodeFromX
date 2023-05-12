875b1b4e1f8e1748a831932c66327943af01783e
Venkey04667 <s.venkataramana0353@gmail.com>
2023-05-11 22:45:27 -0700

    this is commit from X guy

--- src/test/java/Git/AddUser.java
+++ src/test/java/Git/AddUser.java
@@ -6,7 +6,7 @@ import org.testng.annotations.Test;
 public class AddUser {
 
 		@Test
-		public void NewUser()
+		public void NewUser1()
 		{
 			given()
 				.contentType("application/json")
@@ -21,4 +21,22 @@ public class AddUser {
 			.then()
 				.log().all();
 		}
+		
+		@Test
+		public void NewUser2()
+		{
+			given()
+				.contentType("application/json")
+				.body("{\r\n"
+						+ "    \"name\": \"venkey\",\r\n"
+						+ "    \"role\": \"qa\",\r\n"
+						+ "    \"age\": 28,\r\n"
+						+ "    \"city\": \"hyd\"\r\n"
+						+ "}")
+			.when()
+				.post("http://localhost:3000/posts/")
+			.then()
+				.log().all();
+		}
 }
+
